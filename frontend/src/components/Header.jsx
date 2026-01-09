2fc45764a090382e251a268bc83c6eddf4f4894d
huy.pham <phamhuy09042002@gmail.com>
2026-01-09 17:39:29 -0500

    add raw page

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div className="space-y-2 text-center">
+      <h1 className="text-4xl font-bold text-transparent bg-primary bg-clip-text">TodoX</h1>
+      <p className="text-muted-foreground">It does not matter how slowly you go so long as you do not stop.</p>
+    </div>
+  );
+};
+
+export default Header;
